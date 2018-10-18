4802c16001ca8c975d08d7d311be4f9a57b3480f
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-10-18 17:41:57 +0300

    Kapt: Using 'kapt' configuration without the 'kotlin-kapt' plugin applied should throw an error (KT-26145)

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/Kapt3KotlinGradleSubplugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/Kapt3KotlinGradleSubplugin.kt
@@ -99,7 +99,7 @@ class Kapt3KotlinGradleSubplugin : KotlinGradleSubplugin<KotlinCompile> {
         val KAPT_SUBPLUGIN_ID = "org.jetbrains.kotlin.kapt3"
 
         fun getKaptConfigurationName(sourceSetName: String): String {
-            return if (sourceSetName != "main")
+            return if (sourceSetName != SourceSet.MAIN_SOURCE_SET_NAME)
                 "$MAIN_KAPT_CONFIGURATION_NAME${sourceSetName.capitalize()}"
             else
                 MAIN_KAPT_CONFIGURATION_NAME
@@ -121,7 +121,7 @@ class Kapt3KotlinGradleSubplugin : KotlinGradleSubplugin<KotlinCompile> {
             return hasProperty(INFO_AS_WARNINGS) && property(INFO_AS_WARNINGS) == "true"
         }
 
-        fun findMainKaptConfiguration(project: Project) = project.findKaptConfiguration(MAIN_KAPT_CONFIGURATION_NAME)
+        fun findMainKaptConfiguration(project: Project) = project.findKaptConfiguration(SourceSet.MAIN_SOURCE_SET_NAME)
 
         fun createAptConfigurationIfNeeded(project: Project, sourceSetName: String): Configuration {
             val configurationName = Kapt3KotlinGradleSubplugin.getKaptConfigurationName(sourceSetName)
